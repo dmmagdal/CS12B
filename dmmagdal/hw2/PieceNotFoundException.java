61a2b158e678d2aa8ed3c461eee34daf77248b06
Diego Magdaleno <dmmagdal@ucsc.edu>
2018-01-08 10:56:06 -0800

    DONT TAKE MY CODE!

--- dmmagdal/hw2/PieceNotFoundException.java
+++
@@ -1,5 +0,0 @@
-public class PieceNotFoundException extends RuntimeException{
-   public PieceNotFoundException(String s){
-      super(s);
-   }
-}
--- dmmagdal/lab1/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-// HelloWorld.java
-// prints "hello world", first program ever
-class HelloWorld{
-	static public void main(String[] args){
-		System.out.println("hello world");
-	}
-
-}
